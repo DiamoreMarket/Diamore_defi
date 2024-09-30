85eca3b2c97ecbcc4fa8ae4b2dd35fba7d71ada2
gbortnikov <g_bortnikov@list.ru>
2024-09-30 14:28:16 +0700

    test

--- test/StakingNFT.t.sol
+++ test/StakingNFT.t.sol
@@ -1,13 +1,14 @@
 import {Test, console, Vm} from 'forge-std/src/Test.sol';
 import {VmSafe} from 'node_modules/forge-std/src/Vm.sol';
 import {IERC20, SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import {INFTify721} from 'contracts/mock/INFTify721.sol';
+import {NFTify721} from 'contracts/nft/NFTify721.sol';
 import {StakingNFT, IStakingNFT} from 'contracts/staking/StakingNFT.sol';
+import {Treasure} from 'contracts/staking/Treasure.sol';
 
 contract StakingNFTTest is Test {
     using SafeERC20 for IERC20;
 
-    address treasure = makeAddr('treasure');
+    address adminTreasure = makeAddr('adminTreasure');
     address admin = makeAddr('admin');
     address deployer = makeAddr('deployer');
     VmSafe.Wallet validator = vm.createWallet('validator');
@@ -17,21 +18,30 @@ contract StakingNFTTest is Test {
     address controller = 0x25f2F80D9a45B641bEf25342A1B2a0Ae48F78539;
     IERC20 constant usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
-    INFTify721 collection = INFTify721(0x20B7287A72c68602a6b9E3b7F0D8AC0E1b02d2b4);
+    NFTify721 collection = NFTify721(0x20B7287A72c68602a6b9E3b7F0D8AC0E1b02d2b4);
     StakingNFT staking;
+    Treasure treasureContract;
 
     function setUp() public {
         uint256 forkId = vm.createFork(vm.envString('ALCHEMY_RPC'), 20008600);
         vm.selectFork(forkId);
 
         vm.startPrank(deployer);
-
-        staking = new StakingNFT(address(collection), validator.addr, address(usdt), treasure);
+        staking = new StakingNFT(address(collection), validator.addr, address(usdt));
+        treasureContract = new Treasure(admin, address(collection), address(staking), address(usdt));
+        staking.grantRole(staking.ADMIN_ROLE(), admin);
+        treasureContract.grantRole(staking.ADMIN_ROLE(), adminTreasure);
         vm.stopPrank();
-        deal(address(usdt), treasure, 10000e6);
 
-        vm.startPrank(treasure);
-        IERC20(usdt).forceApprove(address(staking), 10000e6);
+        vm.startPrank(admin);
+        staking.setTreasure(address(treasureContract));
+        vm.stopPrank();
+
+        vm.startPrank(adminTreasure);
+        uint256 amount = 10000e6;
+        deal(address(usdt), adminTreasure, amount);
+        usdt.forceApprove(address(treasureContract), amount);
+        treasureContract.depositErc20(amount, address(usdt));
         vm.stopPrank();
     }
 
@@ -43,17 +53,13 @@ contract StakingNFTTest is Test {
         vm.stopPrank();
 
         vm.startPrank(holder);
-
         vm.expectRevert(bytes('ERC721: caller is not token owner or approved'));
         staking.stake(tokenId, IStakingNFT.LockPeriod.ThreeMonths);
-
         _stake(tokenId);
 
         IStakingNFT.NFTInfo memory info = staking.getInfoNFT(tokenId);
-
         assertEq(info.owner, holder);
         assertEq(info.timeUnlock, staking.THREE_MONTH() + block.timestamp);
-        assertEq(info.rewardReceived, 0);
         assertEq(collection.balanceOf(holder), 0);
 
         vm.stopPrank();
@@ -91,19 +97,15 @@ contract StakingNFTTest is Test {
 
         vm.startPrank(holder);
         staking.claim(vrs, message);
+        assertEq(IERC20(usdt).balanceOf(holder), message.amount);
 
         vm.expectRevert(abi.encodeWithSelector(IStakingNFT.HashUsed.selector));
         staking.claim(vrs, message);
 
-        IStakingNFT.NFTInfo memory info = staking.getInfoNFT(tokenId);
-        assertEq(info.rewardReceived, 100e6);
-        assertEq(IERC20(usdt).balanceOf(holder), message.amount);
-
         message = IStakingNFT.Message({tokenId: tokenId, amount: 100e6, nonce: 1});
         vrs = _sign(message, validator);
         staking.claim(vrs, message);
-        info = staking.getInfoNFT(tokenId);
-        assertEq(info.rewardReceived, 200e6);
+        assertEq(IERC20(usdt).balanceOf(holder), message.amount * 2);
     }
 
     function test_unstake() public {
@@ -112,35 +114,23 @@ contract StakingNFTTest is Test {
         _allowTransfer(tokenId);
         _stake(tokenId);
 
-        IStakingNFT.Message memory message = IStakingNFT.Message({tokenId: tokenId, amount: 100e6, nonce: 0});
-        IStakingNFT.Vrs memory vrs = _sign(message, validator);
+        vm.expectRevert(abi.encodeWithSelector(IStakingNFT.NotOwner.selector));
+        staking.unstake(tokenId);
 
         vm.startPrank(holder);
         IStakingNFT.NFTInfo memory info = staking.getInfoNFT(tokenId);
 
         vm.expectRevert(abi.encodeWithSelector(IStakingNFT.NotReadyToUnstake.selector, info.timeUnlock));
-        staking.unstake(vrs, message);
-
+        staking.unstake(tokenId);
         vm.warp(block.timestamp + 100 days);
-        staking.unstake(vrs, message);
-
-        assertEq(IERC20(usdt).balanceOf(holder), message.amount);
+        _unstake(tokenId);
+        assertEq(IERC20(usdt).balanceOf(holder), 0);
         assertEq(collection.balanceOf(holder), 1);
-
-        message = IStakingNFT.Message({tokenId: tokenId, amount: 100e6, nonce: 1});
-        vrs = _sign(message, validator);
-        vm.expectRevert(abi.encodeWithSelector(IStakingNFT.NotOwner.selector));
-        staking.unstake(vrs, message);
-
-        vm.expectRevert(abi.encodeWithSelector(IStakingNFT.NotOwner.selector));
-        staking.claim(vrs, message);
-
-        vm.stopPrank();
 
         info = staking.getInfoNFT(tokenId);
         assertEq(info.timeUnlock, 0);
-        assertEq(info.rewardReceived, 0);
         assertEq(info.owner, address(0));
+        vm.stopPrank();
     }
 
     function test_admin_functions() public {
@@ -148,25 +138,28 @@ contract StakingNFTTest is Test {
         staking.grantRole(staking.ADMIN_ROLE(), admin);
         vm.stopPrank();
 
-        vm.startPrank(admin);
-        staking.setTreasure(address(0));
-        assertEq(staking.treasure(), address(0));
-        vm.stopPrank();
-
         uint256 tokenId = 10475930865216964207240693939129450693466902451350659726041528447710005539241;
         _allowTransfer(tokenId);
         _stake(tokenId);
-        assertEq(collection.balanceOf(address(staking)), 1);
+        assertEq(collection.balanceOf(address(treasureContract)), 1);
 
         vm.startPrank(admin);
-        staking.withdraw(tokenId, admin);
+        treasureContract.withdrawNft(tokenId, admin);
         assertEq(collection.balanceOf(admin), 1);
+        assertEq(collection.balanceOf(address(treasureContract)), 0);
 
-        collection.approve(address(staking), tokenId);
-        staking.refund(tokenId);
-        assertEq(collection.balanceOf(address(staking)), 1);
+        collection.approve(address(treasureContract), tokenId);
+        treasureContract.refundNft(tokenId);
+        assertEq(collection.balanceOf(address(treasureContract)), 1);
         assertEq(collection.balanceOf(admin), 0);
 
+        vm.stopPrank();
+        vm.warp(block.timestamp + 100 days);
+        _unstake(tokenId);
+
+        vm.startPrank(admin);
+        staking.setTreasure(address(0));
+        assertEq(staking.treasure(), address(0));
         vm.stopPrank();
     }
 
@@ -184,10 +177,17 @@ contract StakingNFTTest is Test {
         vm.stopPrank();
     }
 
+    function _unstake(uint256 tokenId) internal {
+        vm.startPrank(holder);
+        staking.unstake(tokenId);
+        vm.stopPrank();
+    }
+
     function _stake(uint256 tokenId) internal {
         vm.startPrank(holder);
         collection.approve(address(staking), tokenId);
         staking.stake(tokenId, IStakingNFT.LockPeriod.ThreeMonths);
+        assertEq(collection.ownerOf(tokenId), address(treasureContract));
         vm.stopPrank();
     }
 }
